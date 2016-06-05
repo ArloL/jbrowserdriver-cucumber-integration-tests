06fa577a9bd4dd691141c1a7792a1739e905aac5
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-06-05 12:42:24 +0200

    Initial import

---
+++ src/main/java/io/github/arlol/JbrowserdriverCucumberIntegrationTestsApplication.java
@@ -0,0 +1,12 @@
+package io.github.arlol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbrowserdriverCucumberIntegrationTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbrowserdriverCucumberIntegrationTestsApplication.class, args);
+	}
+}
