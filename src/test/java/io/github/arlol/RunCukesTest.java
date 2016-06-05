9074003d1fc9a4a0e7f9a610efa91a03e8a837df
Arlo O'Keeffe <mailtoarlo@gmail.com>
2016-06-05 13:41:59 +0200

    Use a classpath folder

--- src/test/java/io/github/arlol/RunCukesTest.java
+++ src/test/java/io/github/arlol/RunCukesTest.java
@@ -6,6 +6,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(features = "classpath:features")
 public class RunCukesTest {
 }
